d04321c3612bb2a40c4c26d90422cac3fbd89e9b
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-01-20 04:02:48 -0500

    adding spring security, actuator, and DevTools

--- src/main/java/com/playground/springboot/myApp/rest/RestControllerEx.java
+++ src/main/java/com/playground/springboot/myApp/rest/RestControllerEx.java
@@ -1,7 +1,36 @@
 package com.playground.springboot.myApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class RestControllerEx {
 
-public class RestController {
+    //inject properties for leader and team names
+
+    @Value("${leader.name}")
+    private String leaderName;
+    @Value("${team.name}")
+    private String teamName;
+
     //expose "/" that returns "Hello World"
+    @GetMapping("/")    //this is a mapping to the root of the application
+    public String sayHello(){
+        return "Hello World";
+    }
 
+    // expose a new endpoint for "team info"
+    @GetMapping("/teamInfo")
+    public String getTeamInfo(){
+        return "Coach: " + leaderName + ", Team name: " + teamName;
+    }
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day!";
+    }
 }
